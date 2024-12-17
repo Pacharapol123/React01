86d5d5fa418b987e0bf524420a9c25375aa023ba
james <6710110276@psu.ac.th>
2024-12-17 21:55:54 +0700

    Test change button

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,56 +1,49 @@
 import React from "react"
-import { Button, Table, Space, Tag, Popconfirm, Modal } from "antd"
+import { Button, Table, Space, Tag, Popconfirm, Modal, Flex } from "antd";
 import { DeleteOutlined, BugOutlined } from '@ant-design/icons';
 import dayjs from "dayjs";
 
 export default function TransactionList(props) {
 
   const columns = [
-    { 
-      title: "Date-Time", 
-      dataIndex: "action_datetime", 
+    {
+      title: "Date-Time",
+      dataIndex: "action_datetime",
       key: "action_datetime",
       render: (_, record) => dayjs(record.action_datetime).format("DD/MM/YYYY - HH:mm")
     },
-    { 
+    {
       title: "Type", dataIndex: "type", key: "type", render: (_, record) => (
         <Tag color={record.type === "income" ? 'green' : 'red'}>{record.type}</Tag>
-      ) 
+      )
     },
     { title: "Amount", dataIndex: "amount", key: "amount" },
     { title: "Note", dataIndex: "note", key: "note" },
     {
       title: "Action", key: "action", render: (_, record) => (
         <Space size="middle">
-          
+
           <Popconfirm
             title="Delete the transaction"
             description="Are you sure to delete this transaction?"
             onConfirm={() => props.onRowDeleted(record.id)}
           >
-            <Button danger 
-              type="primary" 
-              shape="circle" 
+            <Button danger
+              type="primary"
+              shape="circle"
               icon={<DeleteOutlined />} />
           </Popconfirm>
-          <Button 
-            type="primary" 
-            shape="circle" 
-            icon={<BugOutlined/>} 
-            onClick={() => {
-              Modal.info({
-                title: "Debug",
-                content: JSON.stringify(record)
-              })
-            }}/>
+          <Flex gap="small" wrap>
+          <Button>Edit</Button>
+          </Flex>
         </Space>
-      ), 
+      ),
     },
   ]
 
   return (
     <>
-    <Table columns={columns} dataSource={props.data}/>
+      <Table columns={columns} dataSource={props.data} />
     </>
   )
 }
